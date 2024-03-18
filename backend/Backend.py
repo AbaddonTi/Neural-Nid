a99e753a9278c799e2c9acacc63f06432531029b
AbaddonTi <ctekta@gmail.com>
2024-03-18 19:36:11 +0100

    Up v1.1.9.2

--- backend/Backend.py
+++ backend/Backend.py
@@ -32,8 +32,6 @@ def get_device_info(user_agent):
     device_type = "Other"
     if device.is_mobile():
         device_type = "Mobile"
-    elif device.isTablet():
-        device_type = "Tablet"
     elif device.is_desktop():
         device_type = "PC"
 
