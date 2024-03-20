9aa921e3ff1539d0daab850e094c4f1fcd5a31de
AbaddonTi <ctekta@gmail.com>
2024-03-20 18:02:35 +0100

    Up v1.2.3

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -4,17 +4,39 @@
 document.addEventListener("DOMContentLoaded", function() {
     var form = document.getElementById('new-message-form');
     var input = document.getElementById('new-message-input');
+
     if (/iPhone/i.test(navigator.userAgent)) {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
 
-        form.style.backgroundColor = "rgb(239,240,245)";
+            form.style.backgroundColor = "rgb(55,55,55)";
+            input.style.backgroundColor = "rgb(77,77,77)";
+            input.style.color = "rgb(255,255,255)";
+            input.style.borderColor = "rgb(90,90,90)";
+        } else {
+            form.style.backgroundColor = "rgb(239,240,245)";
+            input.style.backgroundColor = "rgb(255,255,255)";
+            input.style.color = "rgb(0,0,0)";
+            input.style.borderColor = "rgb(200,200,200)";
+        }
+    }
+});
 
-        input.style.backgroundColor = "rgb(104,107,108)";
-
+window.matchMedia('(prefers-color-scheme: dark)').addListener(function(e) {
+    var form = document.getElementById('new-message-form');
+    var input = document.getElementById('new-message-input');
+    if (e.matches) {
+        form.style.backgroundColor = "rgb(55,55,55)";
+        input.style.backgroundColor = "rgb(77,77,77)";
         input.style.color = "rgb(255,255,255)";
-
+        input.style.borderColor = "rgb(90,90,90)";
+    } else {
+        form.style.backgroundColor = "rgb(239,240,245)";
+        input.style.backgroundColor = "rgb(255,255,255)";
+        input.style.color = "rgb(0,0,0)";
         input.style.borderColor = "rgb(200,200,200)";
     }
 });
+
 // endregion DOMContentLoaded
 
 
